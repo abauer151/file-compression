0a6d40e244f5730f60f900756f1427375d560211
Aidan Bauer <abauer151@gmail.com>
2023-05-05 15:00:14 -0600

    version 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        System.out.println("Version 2");
+        System.out.println("Version 3");
     }
 }
