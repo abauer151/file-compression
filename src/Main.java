118e265a311516e871187662b2bb683ec32c359f
Aidan Bauer <abauer151@gmail.com>
2023-05-05 18:13:01 -0600

    hello:

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("version 3");
+        System.out.println("version 4");
 
         System.out.println("hello remote");
     }
